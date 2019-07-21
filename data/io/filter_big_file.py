c5ccfeb94d7a2a47a20193b3bd34f273acb10b8c
yaoyaolingshiyan <zmy1176520416@163.com>
2019-07-21 13:01:43 +0800

    update filter_big_file.py

--- data/io/filter_big_file.py
+++ data/io/filter_big_file.py
@@ -2,30 +2,31 @@ import os
 import glob
 
 
-big_file_list = []
-xml_list = glob.glob('D:/competition/crop_dataset/without_big_train/train/labeltxt/*.xml')
-print(len(xml_list))
-for i, xml in enumerate(xml_list):
-    if os.path.getsize(xml) > 103000:
-        big_file_list.append(xml.split('\\')[-1])
-        # print(i, ':', xml)
-print('out of range files have :', len(big_file_list))
+def filter_big_file(max_file_size):
+    big_file_list = []
+    xml_list = glob.glob('D:/competition/crop_dataset/train/labeltxt/*.xml')
+    print(len(xml_list))
+    for i, xml in enumerate(xml_list):
+        if os.path.getsize(xml) > max_file_size:
+            big_file_list.append(xml.split('\\')[-1])
+            print(i, ':', xml)
+    print('out of range files have :', len(big_file_list))
 
-delete_path = 'D:/competition/crop_dataset/without_big_train/train'
-print(big_file_list)
-for j, xm in enumerate(big_file_list):
-    img_name = xm.split('.')[0]+'.png'
-    # print(xm)
-    # print(img_name)
-    img_path = os.path.join(delete_path, 'images', img_name)
-    xml_path = os.path.join(delete_path, 'labeltxt', xm)
-    if os.path.exists(img_path) and os.path.exists(xml_path):
-        os.remove(img_path)
-        os.remove(xml_path)
-        print(j, ' ', img_name, 'have deleted!')
-    else:
-        print(j, ' ', img_name, 'didnot found!')
-        break
+    delete_path = 'D:/competition/crop_dataset/train'
+    print(big_file_list)
+    for j, xm in enumerate(big_file_list):
+        img_name = xm.split('.')[0] + '.png'
+        # print(xm)
+        # print(img_name)
+        img_path = os.path.join(delete_path, 'images', img_name)
+        xml_path = os.path.join(delete_path, 'labeltxt', xm)
+        if os.path.exists(img_path) and os.path.exists(xml_path):
+            os.remove(img_path)
+            os.remove(xml_path)
+            print(j, ' ', img_name, 'have deleted!')
+        else:
+            print(j, ' ', img_name, 'did not found!')
+            break
 
 
 # 尝试删除文件
@@ -37,3 +38,6 @@ for j, xm in enumerate(big_file_list):
 #         os.remove(im)
 # img_path_list = glob.glob(src_path)
 # print(len(img_path_list))
+
+if __name__ == '__main__':
+    filter_big_file(123000)
